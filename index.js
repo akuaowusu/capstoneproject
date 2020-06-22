1689406e7926da0993dd3ffa423cf06b8ace03cf
Pranjal <mahesh@u58233-Pranjals-MacBook-Pro.local>
2020-06-23 02:22:50 +0530

    audio code

--- index.js
+++ index.js
@@ -1,39 +1,13 @@
-function playRain () {
-  var rain = new Audio("rain.mp3");
-  rain.play();
+var counter = 0
+function playAudio(audio){
+    var audio1 = document.getElementById(audio);
+    if (counter % 2 === 0){
+        audio1.play();
+        counter += 1;
+    }
+    else {
+        audio1.pause();
+        counter += 1;
+    }
 }
-
-$('#rain').on('click', function() {
-  playRain();
-  console.log("rain")
-});
-
-function playWind () {
-  var wind = new Audio("wind.mp3");
-  wind.play();
-}
-
-$('#wind').on('click', function() {
-  playWind();
-  console.log("wind")
-});
-
-function playBirds () {
-  var birds = new Audio("birds.mp3");
-  birds.play();
-}
-
-$('#birds').on('click', function() {
-  playBirds();
-  console.log("birds")
-});
-
-function playCampfire () {
-  var campfire = new Audio("campfire.mp3");
-  campfire.play();
-}
-
-$('#campfire').on('click', function() {
-  playCampfire();
-  console.log("campfire")
-});
+// 
